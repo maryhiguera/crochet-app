e2809434596e5f30882ecf065f43b19e558fe170
maryhiguera <marylin0higuera@gmail.com>
2025-12-19 15:42:16 -0700

    adding seeds, routes, crud, and testing

--- app/controllers/patterns_controller.rb
+++ app/controllers/patterns_controller.rb
@@ -1,2 +1,26 @@
 class PatternsController < ApplicationController
+  def index
+    @patterns = Pattern.all
+  end
+
+  def show
+    @pattern = Pattern.find(params[:id])
+  end
+
+  def new
+    @pattern = Pattern.new
+  end
+
+  def create
+    @pattern = Pattern.new(pattern_params)
+    if @pattern.save
+      redirect_to @pattern, notice: "Pattern was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def pattern_params
+    params.require(:pattern).permit(:title, :designer_name, :introduction, :construction_overview, :notes)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  resources :patterns
+  resources :patterns, only: [ :index, :show, :new, :create ]
+  root "patterns#index"
 end
